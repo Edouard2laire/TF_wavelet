91108d61f3bdde98912b87e3bc0ba082d00e6125
Edouard <edouard.delaire@icloud.com>
2025-06-16 20:55:43 -0400

    Improve Apply measure function

--- functions/be_apply_measure.m
+++ functions/be_apply_measure.m
@@ -1,16 +1,20 @@
 function [power, title_tf] = be_apply_measure(tf, OPTIONS)
 
     ofs     = ceil(0.02*size(tf,3));
+
     power   = zeros(size(tf));
+    power(:,:,2+ofs:end-1-ofs) = 0.5*abs(tf(:,:,2+ofs:end-1-ofs)).^2;
+
+    title_tf = 'Time-frequency Amplitude';
 
     if isfield(OPTIONS.wavelet.display,'TaegerK') && strcmp(OPTIONS.wavelet.display.TaegerK,'yes') 
-        tf1 = 0.25*tf(:,:,3:end).*conj(tf(:,:,1:end-2))+0.25*conj(tf(:,:,3:end)).*tf(:,:,1:end-2);       
-        power(:,:,2+ofs:end-1-ofs) = 0.5*abs(tf(:,:,2+ofs:end-1-ofs)).^2 - tf1(:,:,1+ofs:end-ofs);
-        title_tf = 'Time-frequency Amplitude (Taeger-Kaiser normalisation)';
-    else
-        power(:,2+ofs:end-1-ofs) = 0.5*abs(tf(:,2+ofs:end-1-ofs)).^2;
-        title_tf = 'Time-frequency Amplitude (no normalisation)';
-    end
 
+        for t = (2+ofs):( size(tf,3)-1-ofs)
+            tf1 = 0.25*tf(:,:,t+1).*conj(tf(:,:,t-1))+0.25*conj(tf(:,:,t+1)).*tf(:,:,t-1);       
+            power(:,:,t) = power(:,:,t)  - tf1;
+        end
+
+        title_tf = [title_tf, '(Taeger-Kaiser normalisation)'];
+    end
 
 end
